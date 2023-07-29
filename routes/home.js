9ddca2cc109b05a0ec640ae1ee9b597f7c090f74
Abdelrahman435 <ah0383723@gmail.com>
2023-07-29 12:59:56 +0300

    m

--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const { showCategories, showTopCourses, showTopMentors }= require('../controllers/homeController')
 router.get('/getTopCourses', showTopCourses)
-
+router.get('/', (req,res) => res.render('index', {title: 'Express'}));
 module.exports = router;
 
 
