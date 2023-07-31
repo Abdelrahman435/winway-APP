494d05b2e113ceb63506fcce18c5afd6203f6864
Abdelrahman435 <ah0383723@gmail.com>
2023-07-31 23:02:25 +0300

    last

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { showCategories, showTopCourses, showTopMentors }= require('../controllers/homeController')
-router.get('/getTopCourses', showTopCourses)
-router.get('/', (req,res) => res.render('index', {title: 'Express'}));
+const upload = require("../middleware/uploadFiles");
+
+const {
+  showCategories,
+  showTopCourses,
+  showTopMentors,
+  addMentor,
+  getCourse,
+} = require("../controllers/homeController");
+router.get("/getTopCourses", showTopCourses);
+router.get("/getTopMentors", showTopMentors);
+router.post("/create",upload.single('image') ,addMentor);
+router.get("/", (req, res) => res.render("index", { title: "Express" }));
+router.get("/course/:id", getCourse);
+
 module.exports = router;
-
-
--- services/coursesService.js
+++ services/coursesService.js
@@ -9,4 +9,11 @@ async function getTopCourses() {
   return course;
 }
 
-module.exports = { getTopCourses };
+async function getcourse(id){
+  const query = util.promisify(connection.query).bind(connection);
+  const reviews = await query("SELECT COUNT(*) AS totalReviews, AVG(rating) AS avgRating FROM reviews WHERE courseId = ?", [id]);
+  const course = await query("SELECT * FROM courses WHERE id = ?", [id]);
+  return { ...course[0], ...reviews[0] };
+}
+
+module.exports = { getTopCourses, getcourse};
